dc09ca5162030984dd27bb48d8fde71d564bf624
gutolive09 <gutolive01@gmail.com>
2023-08-31 10:54:09 -0300

    Tabela de produtos criada com estrutura html.

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -3,7 +3,24 @@ import React from 'react'
 export default function Produtos() {
   return (
     <>
-        <h1>Produtos</h1>
+        <h1>Produtos Informáticos - FIAPO</h1>
+
+        <table>
+          <tr>
+            <th>ID</th>
+            <th>NOME</th>
+            <th>DESCRIÇÃO</th>
+            <th>PREÇO</th>
+          </tr>
+          <tbody>
+
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={3}>PRODUTOS</td>
+            </tr>
+          </tfoot>
+        </table>
     </>
   )
 }
