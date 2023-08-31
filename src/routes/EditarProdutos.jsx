170165d018723bd00b9f0c6669ac3445c5120186
gutolive09 <gutolive01@gmail.com>
2023-08-31 12:01:57 -0300

    Exibindo dados dos objetos.

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { ListaProdutos } from '../components/ListaProdutos';
 
 export default function EditarProdutos() {
+
+  const {id} = useParams();
+
+  const produtoRecuperado = ListaProdutos.filter((produto) => produto.id == id)[0]
+
+  console.log(produtoRecuperado)
+
   return (
     <>
-        <div>EditarProdutos</div>
+        <h1>EditarProdutos</h1>
+        <h2>PRODUTO</h2>
+        <p>NOME : {produtoRecuperado.nome}</p>
+        <p>DESCRIÇÃO : {produtoRecuperado.desc}</p>
+        <p>PREÇO : {produtoRecuperado.preco}</p>
+
     </>
     
   )
