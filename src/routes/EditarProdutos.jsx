0b691affe76a8f29dcd5d2452b9bcc2415faed4c
gutolive09 <gutolive01@gmail.com>
2023-09-06 12:18:32 -0300

    Recuperando o objeto completo com useParam()

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -4,17 +4,37 @@ import { ListaProdutos } from '../components/ListaProdutos';
 
 export default function EditarProdutos() {
 
+  document.title = "EDITAR PRODUTO"
+
   const {id} = useParams();
 
   const produtoRecuperado = ListaProdutos.filter((produto) => produto.id == id)[0]
 
   return (
     <>
-        <h1>EditarProdutos</h1>
-        <h2>PRODUTO</h2>
-        <p>NOME : {produtoRecuperado.nome}</p>
-        <p>DESCRIÇÃO : {produtoRecuperado.desc}</p>
-        <p>PREÇO : {produtoRecuperado.preco}</p>
+        <div>
+          <form>
+            <fieldset>
+              <legend>Produto Selecionado</legend>
+              <div>
+                <label htmlFor="idNome">Nome: </label>
+                <input type="text" name='nome' id='idNome' defaultValue={produtoRecuperado.nome}/>
+              </div>
+              <div>
+                <label htmlFor="idDesc">Descrição: </label>
+                <input type="text" name='desc' id='idDesc' defaultValue={produtoRecuperado.desc}/>
+              </div>
+              <div>
+                <label htmlFor="idPreco">Preço: </label>
+                <input type="text" name='preco' id='idPreco' defaultValue={produtoRecuperado.preco}/>
+              </div>
+            </fieldset>
+            <div>
+              <button>Editar</button>
+            </div>
+          </form>
+        </div>
+
 
     </>
     
